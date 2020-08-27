8cf86f31f2d29ee146f90dfd1500325956fd4a5a
Jamin <jamin@se7enet.com>
2020-08-26 21:21:10 -0500

    + Create Secrets table

---
+++ database/migrations/2020_08_26_194931_create_secrets_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSecretsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('secrets', function (Blueprint $table) {
+            $table->id();
+            $table->string('slug', 32)->unique('UX_secrets_slug')->index('IX_secrets_slug');
+            $table->text('secret')->nullable();
+            $table->timestamps();
+            $table->datetime('expires_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('secrets');
+    }
+}
