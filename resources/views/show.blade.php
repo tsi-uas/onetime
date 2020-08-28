b92b58dec5a9d6f4654bd1b14fba1dd4275c6093
Jamin <jamin@se7enet.com>
2020-08-27 20:03:23 -0500

    ~ Add failure handling, and  remove the setSelectionRange thing

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -12,12 +12,14 @@
         $('#load i').removeClass('fa-user-secret').addClass('fa-sync-alt fa-spin').attr('disabled', 'disabled');
         $.get('{{ route('load', $secret) }}', function (secret) {
             $('#show').html(secret);
+            $('#copy').click(copySecret);
+        }).fail(function () {
+            $('#show').html('<p class="text-danger">Whoops, that secret has already been destroyed.</p><p><a class="btn btn-primary" href="{{ route('home') }}"><i class="fas fa-redo fa-fw"></i> Share Another Secret</a></p>');
         });
     }
     function copySecret() {
         var $secret = $('#secret');
         $secret.select();
-        $secret.setSelectionRange(0, 99999);
         document.execCommand("copy");
     }
 </script>
